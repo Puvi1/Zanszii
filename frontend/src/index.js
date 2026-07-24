299e399fe59120e7c38b2b8d175af5efea3e74a4
Puviyarasu <108527641+Puvi1@users.noreply.github.com>
2026-07-24 19:54:15 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 
-import "/index.css";
-import App from "/App";
+import "./index.css";
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
