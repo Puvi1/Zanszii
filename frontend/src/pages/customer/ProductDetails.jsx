88ca5f8af4523c994a08706920296628b8e1ff45
Puviyarasu <108527641+Puvi1@users.noreply.github.com>
2026-08-03 18:11:25 +0530

    Update ProductDetails.jsx

--- frontend/src/pages/customer/ProductDetails.jsx
+++ frontend/src/pages/customer/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import {
   ArrowLeft,
+  ArrowRight,
   CheckCircle,
   Heart,
   ShareNetwork,
